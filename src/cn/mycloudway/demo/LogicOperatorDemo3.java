136e28345ff8347f24aa29692c9f17b7215ded2a
vancat1968 <fanxinmi@gmail.com>
2023-06-25 21:06:30 +0800

    demo3

--- src/cn/mycloudway/demo/LogicOperatorDemo3.java
+++ src/cn/mycloudway/demo/LogicOperatorDemo3.java
@@ -3,10 +3,18 @@ package cn.mycloudway.demo;
 public class LogicOperatorDemo3 {
     public static void main(String[] args) {
         System.out.println(true && true);
-        System.out.println(false && false);
+
         System.out.println(true && false);
         System.out.println(false && true);
 
+        System.out.println(true || true);
+        System.out.println(false || false);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(true || true);
+        System.out.println(false || false);
+        System.out.println(true || false);
+        System.out.println(false || true);
         System.out.println(true || true);
         System.out.println(false || false);
         System.out.println(true || false);
